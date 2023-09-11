d28e6c20b6d2acc1783fd7321dc8ffbc3685b14d
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-11 11:57:10 -0300

    update: If condition updated

--- src/components/condicional/UsuarioInfo.jsx
+++ src/components/condicional/UsuarioInfo.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import If from './if'
+
 export default props =>{
-
+    const usuario = props.usuario || {}
     return(
-
+        
         <div>
-
-            Seja bem vindo <strong>{ usuario.nome }</strong>!
-
+            <If test={usuario && usuario.nome}>
+            Seja bem vindo <strong>{ props.usuario.nome }</strong>!
+            </If>
+            <If test={usuario && !usuario.nome}>
+            Seja bem vindo <strong>Amigao</strong>!
+            </If>
         </div>
 
     )
