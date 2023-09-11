7846ed7554fbfe601056c8fc476927b5dfc8578b
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-11 11:41:25 -0300

    added: produtos.js

---
+++ src/data/produtos.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        nomeProduto: 'Carne',
+        preco: '200'
+    },
+    {
+        id: 2,
+        nomeProduto: 'Carne de soja',
+        preco: '1'
+    },
+    {
+        id: 3,
+        nomeProduto: 'Sopa',
+        preco: '10'
+    },
+    {
+        id: 4,
+        nomeProduto: 'Frango',
+        preco: '50'
+    },
+]
