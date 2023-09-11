fc0e16678fa59f58209163855703b90805476af5
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-11 11:37:47 -0300

    update: now index.js renders app.jsx

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import ReactDOM from "react-dom";
 import React from 'react';
 import './index.css';
+import App from "./app";
 
-import Primeiro from "./components/basics/Primeiro";
 
-ReactDOM.render(
-<div>
 
-    <Primeiro></Primeiro>
-
-    
-</div>
-
-, document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
