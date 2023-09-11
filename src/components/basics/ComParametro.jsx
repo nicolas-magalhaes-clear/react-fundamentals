478ba6bb95dc86149dc1e7d3f5991159f2b5b96e
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-11 11:38:40 -0300

    added: ComParametro.jsx

---
+++ src/components/basics/ComParametro.jsx
@@ -0,0 +1,8 @@
+export default function ComParametro(props){
+    return (
+        <div>
+            <h2>{ props.titulo } </h2>
+            <h3> { props.aluno } Nota: { props.nota } </h3>
+        </div>
+    )
+}
