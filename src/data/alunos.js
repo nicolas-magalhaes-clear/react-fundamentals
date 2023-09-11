48d3782690ab22c513356f291c1bf93f3c2b09b3
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-11 11:41:19 -0300

    added: alunos.js

---
+++ src/data/alunos.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id:1,
+        nome: 'Lucas',
+        nota: 9.2
+    },
+    {
+        id:2,
+        nome: 'Julia',
+        nota: 7.1
+    },
+    {
+        id:3,
+        nome: 'Roberto',
+        nota: 6.7
+    },
+    {
+        id:4,
+        nome: 'Daniel',
+        nota: 5.3
+    },
+
+]
